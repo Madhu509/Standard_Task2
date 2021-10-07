761d8f91822a3df358c76de5ced9ae97ec6796b5
unknown <you@example.com>
2021-10-07 19:46:28 +1300

    initial commit

---
+++ StandardTask2/Helpers/ConstantHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsQA_1.Helpers
+{
+   public class ConstantHelpers
+    {
+        //Base Url
+        public static string Url = "http://localhost:5000/";
+
+        //ScreenshotPath
+        public static string ScreenshotPath = @"C:\Users\madhu\Desktop\Advance_Task2\StandardTask2\TestReports\Screenshots";
+
+        //ExtentReportsPath
+        public static string ReportsPath = @"C:\Users\madhu\Desktop\Advance_Task2\StandardTask2\TestReports\Reports";
+
+        //ReportXML Path
+        public static string ReportXMLPath = @"C:\Users\madhu\Desktop\Advance_Task2\StandardTask2\TestReports\Reports";
+
+        //ExcelPath
+        public static string ExcelPath = @"C:\Users\madhu\Desktop\Advance_Task2\StandardTask2\Data\Mars.xlsx";
+
+
+    }
+}
---
+++ StandardTask2/Steps/ManageRequetsSteps/SentRequestSteps.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.Feature.ManageRequest
+{
+    [Binding]
+    public class SentRequestSteps
+    {
+        [Given(@"The User Clicks ManageRequest tab")]
+        public void GivenTheUserClicksManageRequestTab()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Select SentRequest DropDown")]
+        public void GivenSelectSentRequestDropDown()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"if User wants to withdraw, click on withdraw button")]
+        public void GivenIfUserWantsToWithdrawClickOnWithdrawButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"if the Status is Accepted the Request is Accepted by Receiver")]
+        public void GivenIfTheStatusIsAcceptedTheRequestIsAcceptedByReceiver()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Completed Button will be Visible")]
+        public void GivenCompletedButtonWillBeVisible()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"if the Status is Completed")]
+        public void GivenIfTheStatusIsCompleted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"if the Status is Declined the Request is Declined")]
+        public void GivenIfTheStatusIsDeclinedTheRequestIsDeclined()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"If the Sender has not Accepted the Status will be pending\.")]
+        public void ThenIfTheSenderHasNotAcceptedTheStatusWillBePending_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the Status should be Withdrawn")]
+        public void ThenTheStatusShouldBeWithdrawn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Click on Completed Button")]
+        public void ThenClickOnCompletedButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Click on Review Button")]
+        public void ThenClickOnReviewButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Add Review Text and Ratings and Save\.")]
+        public void ThenAddReviewTextAndRatingsAndSave_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
