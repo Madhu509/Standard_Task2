fd8b771f2f06c12833ac8b329be4dd3746df04c5
unknown <you@example.com>
2021-10-11 13:34:16 +1300

    final commit

--- StandardTask2/Steps/ManageRequetsSteps/SentRequestSteps.cs
+++ StandardTask2/Steps/ManageRequetsSteps/SentRequestSteps.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarsQA_1.Pages;
+using System;
 using TechTalk.SpecFlow;
 
 namespace MarsQA_1.Feature.ManageRequest
@@ -6,76 +7,85 @@ namespace MarsQA_1.Feature.ManageRequest
     [Binding]
     public class SentRequestSteps
     {
-        [Given(@"The User Clicks ManageRequest tab")]
-        public void GivenTheUserClicksManageRequestTab()
+        [Binding]
+        public class SentRequestStep
         {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Given(@"Select SentRequest DropDown")]
-        public void GivenSelectSentRequestDropDown()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Given(@"if User wants to withdraw, click on withdraw button")]
-        public void GivenIfUserWantsToWithdrawClickOnWithdrawButton()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Given(@"if the Status is Accepted the Request is Accepted by Receiver")]
-        public void GivenIfTheStatusIsAcceptedTheRequestIsAcceptedByReceiver()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Given(@"Completed Button will be Visible")]
-        public void GivenCompletedButtonWillBeVisible()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Given(@"if the Status is Completed")]
-        public void GivenIfTheStatusIsCompleted()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Given(@"if the Status is Declined the Request is Declined")]
-        public void GivenIfTheStatusIsDeclinedTheRequestIsDeclined()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"If the Sender has not Accepted the Status will be pending\.")]
-        public void ThenIfTheSenderHasNotAcceptedTheStatusWillBePending_()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"the Status should be Withdrawn")]
-        public void ThenTheStatusShouldBeWithdrawn()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"Click on Completed Button")]
-        public void ThenClickOnCompletedButton()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"Click on Review Button")]
-        public void ThenClickOnReviewButton()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"Add Review Text and Ratings and Save\.")]
-        public void ThenAddReviewTextAndRatingsAndSave_()
-        {
-            ScenarioContext.Current.Pending();
+
+            SentRequest SdRqst = new SentRequest();
+            RecievedRequest RcRqst = new RecievedRequest();
+
+            [Given(@"The User Clicks ManageRequest tab")]
+            public void GivenTheUserClicksManageRequestTab()
+            {
+                RcRqst.ManageRequest();
+            }
+
+            [Given(@"Select SentRequest DropDown")]
+            public void GivenSelectSentRequestDropDown()
+            {
+                SdRqst.SenRequest();
+            }
+
+            [Then(@"If the Sender has not Accepted the Status will be pending\.")]
+            public void ThenIfTheSenderHasNotAcceptedTheStatusWillBePending_()
+            {
+                SdRqst.PendStatus();
+            }
+
+            [Given(@"if User wants to withdraw, click on withdraw button")]
+            public void GivenIfUserWantsToWithdrawClickOnWithdrawButton()
+            {
+                SdRqst.WithdrawSentRequest();
+            }
+
+            [Then(@"the Status should be Withdrawn")]
+            public void ThenTheStatusShouldBeWithdrawn()
+            {
+                SdRqst.WithdrStatus();
+            }
+            [Given(@"if the Status is Accepted the Request is Accepted by Receiver")]
+            public void GivenIfTheStatusIsAcceptedTheRequestIsAcceptedByReceiver()
+            {
+                SdRqst.Accepted();
+            }
+
+            [Given(@"Completed Button will be Visible")]
+            public void GivenCompletedButtonWillBeVisible()
+            {
+                SdRqst.Cmpletebtn();
+            }
+
+
+            [Then(@"Click on Completed Button")]
+            public void ThenClickOnCompletedButton()
+            {
+                SdRqst.Completed();
+            }
+            [Given(@"if the Status is Completed")]
+            public void GivenIfTheStatusIsCompleted()
+            {
+                SdRqst.CompleteStatus();
+            }
+
+            [Then(@"Click on Review Button")]
+            public void ThenClickOnReviewButton()
+            {
+                SdRqst.ReviewBtn();
+            }
+
+            [Then(@"Add Review Text and Ratings and Save\.")]
+            public void ThenAddReviewTextAndRatingsAndSave_()
+            {
+                SdRqst.Review();
+            }
+
+            [Given(@"if the Status is Declined the Request is Declined")]
+            public void GivenIfTheStatusIsDeclinedTheRequestIsDeclined()
+            {
+                SdRqst.DeclinStatus();
+            }
+
+
         }
     }
 }
